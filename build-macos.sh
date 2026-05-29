2f700ca1293e535efc1161742f3c2795cd14786c
CacahueteSansSel <24205659+CacahueteSansSel@users.noreply.github.com>
2026-05-29 15:47:27 +0200

    reverted macos changes because apple

--- build-macos.sh
+++ build-macos.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 APP_NAME="Powerplant"
-BUILD_DIR="Powerplant/bin/Release/net10.0/osx-arm64/publish"
+BUILD_DIR="Powerplant/bin/Debug/net10.0/osx-arm64/publish"
 APP_DIR="output/$APP_NAME.app"
 
 cd $APP_NAME
-dotnet publish -c Release -f net10.0 -r osx-arm64 --self-contained true
+dotnet publish -c Debug -f net10.0 -r osx-arm64 --self-contained true
 cd ..
 
 mkdir -p "$APP_DIR/Contents/MacOS"
@@ -13,8 +13,9 @@ mkdir -p "$APP_DIR/Contents/Resources"
 cp -R "$BUILD_DIR/"* "$APP_DIR/Contents/MacOS/"
 
 iconutil -c icns Resources/Icon.iconset
+cp Resources/Icon.icns "$APP_NAME/Icon.icns"
 
 cp "$APP_NAME/Info.plist" "$APP_DIR/Contents/Info.plist"
-cp Resources/Icon.icns "$APP_DIR/Contents/Resources/Icon.icns"
+cp "$APP_NAME/Icon.icns" "$APP_DIR/Contents/Resources/Icon.icns"
 
 chmod +x "$APP_DIR/Contents/MacOS/Powerplant"
