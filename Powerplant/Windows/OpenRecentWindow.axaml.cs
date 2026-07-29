69f4d2e3a437a73e3dce187cad2a5f6c62c52e1f
CacahueteSansSel <24205659+CacahueteSansSel@users.noreply.github.com>
2026-07-29 19:50:12 +0200

    new open recent window

--- Powerplant/Windows/OpenRecentWindow.axaml.cs
+++ Powerplant/Windows/OpenRecentWindow.axaml.cs
@@ -1,7 +1,11 @@
+using System;
+using System.IO;
+using System.Linq;
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
+using Avalonia.Media.Imaging;
 using Powerplant.Core;
 
 namespace Powerplant.Windows;
@@ -12,8 +16,16 @@ public partial class OpenRecentWindow : Window
     {
         InitializeComponent();
 
-        foreach (string file in RecentFilesManager.Files)
-            RecentFilesListBox.Items.Add(file);
+        RecentFilesListBox.ItemsSource = RecentFilesManager.Files
+            .Where(File.Exists)
+            .Select(file => new RecentFile(file));
+        
+        Loaded += OnLoaded;
+    }
+
+    private void OnLoaded(object? sender, RoutedEventArgs e)
+    {
+        SearchTextBox.Focus();
     }
 
     private void OpenButtonClicked(object? sender, RoutedEventArgs e)
@@ -23,14 +35,51 @@ public partial class OpenRecentWindow : Window
 
     private void CancelButtonClicked(object? sender, RoutedEventArgs e)
     {
-        
+        Close(null);
     }
 
     private void RecentFilesListBox_OnSelectionChanged(object? sender, SelectionChangedEventArgs e)
     {
         if (e.AddedItems.Count == 0) return;
 
-        string item = (string)e.AddedItems[0];
-        Close(item);
+        RecentFile item = (RecentFile?)e.AddedItems[0];
+        Close(item.FullPath);
+    }
+
+    public class RecentFile
+    {
+        public string FullPath { get; }
+        public string Filename => Path.GetFileName(FullPath);
+        public Bitmap? Thumbnail => GetThumbnail();
+
+        public RecentFile(string fullPath)
+        {
+            FullPath = fullPath;
+        }
+
+        private Bitmap? GetThumbnail()
+        {
+            try
+            {
+                return new Bitmap(FullPath);
+            }
+            catch (Exception e)
+            {
+                return null;
+            }
+        }
+    }
+
+    private void SearchTextBox_OnTextChanged(object? sender, TextChangedEventArgs e)
+    {
+        if (string.IsNullOrWhiteSpace(SearchTextBox.Text))
+        {
+            RecentFilesListBox.ItemsSource = RecentFilesManager.Files.Select(file => new RecentFile(file));
+            return;
+        }
+        
+        RecentFilesListBox.ItemsSource = RecentFilesManager.Files
+            .Where(file => file.Contains(SearchTextBox.Text.Trim(), StringComparison.InvariantCultureIgnoreCase))
+            .Select(file => new RecentFile(file));
     }
 }
