60127561b300d86a979c4f3d1e81cf26aa08be2c
CacahueteSansSel <24205659+CacahueteSansSel@users.noreply.github.com>
2026-08-01 17:02:41 +0200

    added new texture size presets on new texture window

--- Powerplant/Windows/NewTextureWindow.axaml.cs
+++ Powerplant/Windows/NewTextureWindow.axaml.cs
@@ -33,4 +33,34 @@ public partial class NewTextureWindow : Window
     {
         WidthBox.Focus();
     }
+
+    private void TextureSizePresetButton16_OnClick(object? sender, RoutedEventArgs e)
+    {
+        Close(new Vector2(16, 16));
+    }
+
+    private void TextureSizePresetButton32_OnClick(object? sender, RoutedEventArgs e)
+    {
+        Close(new Vector2(32, 32));
+    }
+
+    private void TextureSizePresetButton64_OnClick(object? sender, RoutedEventArgs e)
+    {
+        Close(new Vector2(64, 64));
+    }
+
+    private void TextureSizePresetButton128_OnClick_OnClick(object? sender, RoutedEventArgs e)
+    {
+        Close(new Vector2(128, 128));
+    }
+
+    private void TextureSizePresetButton512x340_OnClick(object? sender, RoutedEventArgs e)
+    {
+        Close(new Vector2(512, 340));
+    }
+
+    private void TextureSizePresetButton256x170_OnClick(object? sender, RoutedEventArgs e)
+    {
+        Close(new Vector2(256, 170));
+    }
 }
